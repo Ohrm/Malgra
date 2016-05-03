4adfeff25692563b408e68b2bee2d9003378fcec
Wehavecookies56 <qwenit56@gmail.com>
2016-05-03 12:30:06 +0100

    Special block can rotate

--- src/main/java/ohrm/malgra/blocks/SpecialBlock.java
+++ src/main/java/ohrm/malgra/blocks/SpecialBlock.java
@@ -2,8 +2,12 @@ package ohrm.malgra.blocks;
 
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.IProperty;
+import net.minecraft.block.properties.PropertyDirection;
+import net.minecraft.block.state.BlockStateContainer;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.EnumFacing;
@@ -16,12 +20,28 @@ import ohrm.malgra.client.sounds.Sounds;
 
 public class SpecialBlock extends Block{
 
+	public static final PropertyDirection FACING = PropertyDirection.create("facing", EnumFacing.Plane.HORIZONTAL);
+
 	public SpecialBlock() {
 		super(Material.DRAGON_EGG);
-		
-		
+		this.setDefaultState(this.blockState.getBaseState().withProperty(FACING, EnumFacing.NORTH));
 	}
-	
+
+	@Override
+	protected BlockStateContainer createBlockState() {
+		return new BlockStateContainer(this, new IProperty[] {FACING});
+	}
+
+	@Override
+	public int getMetaFromState(IBlockState state) {
+		return state.getValue(FACING).getIndex();
+	}
+
+	@Override
+	public IBlockState onBlockPlaced(World worldIn, BlockPos pos, EnumFacing facing, float hitX, float hitY, float hitZ, int meta, EntityLivingBase placer) {
+		return this.getDefaultState().withProperty(FACING, placer.getHorizontalFacing().getOpposite());
+	}
+
 	@Override
 	public boolean isOpaqueCube(IBlockState state) {
 		// TODO Auto-generated method stub
